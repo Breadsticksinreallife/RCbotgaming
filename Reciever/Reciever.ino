fc64c1ac3b8d0f88a7191bfb3abb049e0af64697
Breadsticksinreallife <126720487+Breadsticksinreallife@users.noreply.github.com>
2023-05-02 13:35:10 -0700

    Update Reciever.ino

--- Reciever/Reciever.ino
+++ Reciever/Reciever.ino
@@ -8,13 +8,20 @@ int LMPower;
 int RMPower;
 
 //Motor definition stuff
-#define leftMotor_IN1 2
+/*#define leftMotor_IN1 2
 #define leftMotor_IN2 4
 #define leftMotor_PWM_pin 5  //capped at <0, 255>
 
 #define rightMotor_IN1 7
 #define rightMotor_IN2 8
-#define rightMotor_PWM_pin 6
+#define rightMotor_PWM_pin 6*/
+#define leftMotor_IN1 7
+#define leftMotor_IN2 8
+#define leftMotor_PWM_pin 6  //capped at <0, 255>
+
+#define rightMotor_IN1 2
+#define rightMotor_IN2 4
+#define rightMotor_PWM_pin 5
 #define buttonPin 12
 void LMcw() {
   digitalWrite(leftMotor_IN1, LOW);
@@ -35,23 +42,29 @@ void RMccw() {
 }
 
 void receiveSignal() {
-  while (mySerial.available() < 1);
+  while (mySerial.available() < 1)
+    ;
   address = char(mySerial.read());
   if (address == 1) {
 
-    while (mySerial.available() < 1);
+    while (mySerial.available() < 1)
+      ;
     packet1 = char(mySerial.read());  //Left Motor Power
 
-    while (mySerial.available() < 1);
+    while (mySerial.available() < 1)
+      ;
     packet2 = char(mySerial.read());  //Right Motor Power
 
-    while (mySerial.available() < 1);
+    while (mySerial.available() < 1)
+      ;
     packet3 = char(mySerial.read());  // 0000, LMdir, RMdir, button 1, button 2 -- 0 is off or cw, 1 is on or ccw
 
-    while (mySerial.available() < 1);
+    while (mySerial.available() < 1)
+      ;
     packet4 = char(mySerial.read());
 
-    while (mySerial.available() < 1);
+    while (mySerial.available() < 1)
+      ;
     checksumPacket = char(mySerial.read());
   }
 }
@@ -59,12 +72,27 @@ void receiveSignal() {
 bool verifyChecksum() {
   byte checksumVer = packet1 + packet2 + packet3 + packet4 + checksumPacket;
   Serial.println("-");
-  Serial.println(packet1);
-  Serial.println(packet2);
+  // Serial.println(packet1);
+  // Serial.println(packet2);
   Serial.println(packet3);
-  Serial.println(packet4);
+  // Serial.println(packet4);
   Serial.println(checksumVer);
   return checksumVer == 0;
+}
+void motorControl() {
+  analogWrite(leftMotor_PWM_pin, packet1);
+  analogWrite(rightMotor_PWM_pin, packet2);
+  if ((packet3 & 0b1000) >> 3) {
+    LMccw();
+  } else {
+    LMcw();
+  }
+  if ((packet3 & 0b100) >> 2) {
+    RMccw();
+  } else {
+    RMcw();
+  }
+  //Serial.println("wroking");
 }
 
 void setup() {
@@ -75,23 +103,19 @@ void setup() {
   pinMode(leftMotor_IN2, OUTPUT);
   pinMode(rightMotor_IN1, OUTPUT);
   pinMode(rightMotor_IN2, OUTPUT);
-  packet3 = 0b00001000;
   pinMode(buttonPin, OUTPUT);
-  while (digitalRead(buttonPin) == HIGH)
-    ;
+  //while (digitalRead(buttonPin) == LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-/*    receiveSignal();
-    if (verifyChecksum()){
-      Serial.println("g");
-    } else {
-      Serial.println("b");
-    }*/
-    LMccw();
-    RMccw();
-    analogWrite(leftMotor_PWM_pin, 128);
-    analogWrite(rightMotor_PWM_pin, 128);
-
+  receiveSignal();
+  if (verifyChecksum()) {
+    //Serial.println("g");
+    motorControl();
+  } else {
+    Serial.println("b");
+    analogWrite(leftMotor_PWM_pin, 0);
+    analogWrite(rightMotor_PWM_pin, 0);
+  }
 }
